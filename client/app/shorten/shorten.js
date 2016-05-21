493bf057bcd3f4ca3dccfaf5e1f3472bce422df2
Adam Lessen <lessena@gmail.com>
2016-05-21 11:19:44 -0700

    Add form validation for shorten links

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,29 +3,35 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = {};
-  $scope.errorMessage = false;
+  $scope.test = false;  
 
   $scope.addLink = function() {
     console.log('scope.newLink: ', $scope.newLink);
     
-    if ($scope.isValidUrl($scope.newLink)) {
+    if ($scope.newLink) {
       Links.addOne($scope.newLink);
       $scope.newLink = '';
-      $scope.errorMessage = false;
-    } else {
-      $scope.errorMessage = true;
     }
-    
 
   };
 
+  // $scope.$watch('url.text' , function (newValue, oldValue) {
+  //   console.log('my function is watching the link: ', newValue);
+  //   if (newValue && newValue.length > 0) {
+  //     if ($scope.isValidUrl(newValue)) {
+  //       $scope.test = true;
+  //       $scope.expression = '';
+  //     } else {
+  //       $scope.expression = 'error';
+      // }
+      
+  //   }
+  // });
 
-  $scope.isValidUrl = function (url) {
-    var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
-    return url.match(rValidUrl);
-  };
-
-
+  // $scope.isValidUrl = function (url) {
+  //   var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+  //   return url.match(rValidUrl);
+  // };
 
 
 });
